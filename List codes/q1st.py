dae0487e4263eae5fb629ab4cc95e21b14727381
Harsh Bediya <harshbedi3112@gmail.com>
2026-08-27 23:50:52 +0530

    Add q1st.py with examples of heterogeneous and homogeneous lists in Python

---
+++ List codes/q1st.py
@@ -0,0 +1,20 @@
+l1=[10,20,3.5,"Don",2+5j]
+print(l1)
+print(type(l1))
+
+l2=[10,20,3.5,"Don",2+5j]
+print(l2) #Hetrogeneous
+
+l3=[1,2,3,4]
+print(l3) #Homogeneous
+
+l4=[10,20,30,40,10,10]
+print(l4)
+
+l5=[10,"DON",10.33]
+print(l5[1])
+print(l5[-1])
+
+l6=[10,"DON",10.33,2+4j]
+print(l6[1:3:1])
+
