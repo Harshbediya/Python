5f89ba2a77290e43838cb57cfec425370a5b60d6
Harsh Bediya <harshbedi3112@gmail.com>
2026-08-27 23:58:15 +0530

    Refactor: Comment out unused list examples and enhance slicing examples in q1st.py

--- List codes/q1st.py
+++ List codes/q1st.py
@@ -1,20 +1,28 @@
-l1=[10,20,3.5,"Don",2+5j]
-print(l1)
-print(type(l1))
+# l1=[10,20,3.5,"Don",2+5j]
+# print(l1)
+# print(type(l1))
 
-l2=[10,20,3.5,"Don",2+5j]
-print(l2) #Hetrogeneous
+# l2=[10,20,3.5,"Don",2+5j]
+# print(l2) #Hetrogeneous
 
-l3=[1,2,3,4]
-print(l3) #Homogeneous
+# l3=[1,2,3,4]
+# print(l3) #Homogeneous
 
-l4=[10,20,30,40,10,10]
-print(l4)
+# l4=[10,20,30,40,10,10]
+# print(l4)
 
-l5=[10,"DON",10.33]
-print(l5[1])
-print(l5[-1])
+# l5=[10,"DON",10.33]
+# print(l5[1])
+# print(l5[-1])
 
-l6=[10,"DON",10.33,2+4j]
-print(l6[1:3:1])
+l6=[10,"DON",10.33,2+4j,10,20]
+print(l6[1:4:1])
+
+print(l6[1:4:2])
+print(l6[1:3:])
+
+l7=[10,20,30]
+print(l7)
+l7[0]="iron man"
+print(l7)
 
